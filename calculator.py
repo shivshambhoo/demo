cef0866ceb5fc7d8600a66c1bbcffa6e6d16517f
shivshambhoo chaudhary <shivshambhookch@gmail.com>
2024-10-23 03:53:32 +0530

    second commit

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
 def add_numbers(num1, num2):
     return num1 + num2
-
+def sub_numbers(num1, num2):
+    return num1 - num2
 result = add_numbers(4, 5)
 print(result)  # This will output 9
+sol=sub_numbers(5,4)
+print(sol) # This
